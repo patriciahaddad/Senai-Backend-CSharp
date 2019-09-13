4ab1e14c1117d24d2e3b3f72c4b61470d56490eb
FIC <FIC@info.local>
2019-09-13 09:38:03 -0300

    Arquivos incluídos

---
+++ backend/aula1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Somente declaramos, sem atribuir valor
+            int num1;
+
+            //Declarar e atribuir valor
+            int num2 = 5;
+            
+            //Declarar diversas variáveis do mesmo tipo (posso ou não atribuir valor)
+            int num3, num4, num5 = 7;
+            
+            //Variável do tipo decimal/real
+            float media;
+            
+            //Variável do tipo lógico (booleana) 
+            bool resultado = false;
+
+            num1= 3;
+            Console.WriteLine(num1 + num5);
+
+
+
+
+        }
+    }
+}
