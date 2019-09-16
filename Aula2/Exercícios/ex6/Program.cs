ceb7f0e791322c2ea9ee30f4caf3c3c6e1e2b213
FIC <FIC@info.local>
2019-09-16 16:00:27 -0300

    Exercícios em aula

---
+++ Aula2/Exercícios/ex6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            Console.WriteLine("Digite sua idade:");
+            id = int.Parse(Console.ReadLine());
+            if (id> 3 && id < 6){
+                Console.WriteLine("Você está na categoria Early Childhood");
+            } else if (id >= 6 && id < 10){
+                Console.WriteLine("Você está na categoria Everyone");
+            } else if (id >= 10 && id < 13){
+                Console.WriteLine("Você está na categoria Everyone 10+");
+            } else if (id >= 13 && id < 17){
+                Console.WriteLine("Você está na categoria Teen");
+            } else if (id >= 17 && id < 18){
+                Console.WriteLine("Você está na categoria Mature 17+");
+            } else if (id >= 18){
+                Console.WriteLine("Você está na categoria Adults Only 18+");
+            }
+        }
+    }
+}
