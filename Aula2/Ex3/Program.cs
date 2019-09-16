5b04989bcc7a3b671ce713c147b53d0690b75ef8
FIC <FIC@info.local>
2019-09-16 14:13:55 -0300

    Aula 3

---
+++ Aula2/Ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deixamos declarada como falsa por padrão
+            bool entrada = false;
+            int idade;
+            string retorno;
+            Console.Write("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            //Como já atribuimos anteriormente, mudamos o estado da variável para verdadeira, caso entre nessa condição
+            if (idade > 17){
+                entrada = true;
+            }
+            if (entrada == true) {
+                retorno = "Entrada permitida!";
+            } else {
+                retorno = "Entrada não permitida :/";
+            }
+            Console.WriteLine(retorno);
+
+
+        }
+    }
+}
