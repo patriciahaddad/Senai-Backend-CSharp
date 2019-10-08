5d7d1f9c7d63e78b17915ae5cef3e873d4d8ae1f
FIC <FIC@info.local>
2019-10-08 16:15:11 -0300

    Try Catch

---
+++ Aula7_try_catch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula7_try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  {
+            int numero;
+        try{
+            //Escrevemos um bloco de código
+
+            Console.WriteLine("Digite um número");
+            numero = int.Parse(Console.ReadLine());
+
+        }catch(Exception ex){
+            //Mostramos a exceção da regra - erro gerado na aplicação
+            
+            Console.WriteLine("Ops :( Deu ruim. Erro: " + ex);
+
+        }
+        }
+    }
+    }
+}
+
